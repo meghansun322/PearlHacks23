30249d0bb36db0fdbd1cdfd00bca9d61f9d055c3
Meghan Sun <meghansun322@gmail.com>
2023-02-11 12:56:23 -0500

    created swift project in repo

---
+++ FinancialLiteracyApp/FinancialLiteracyApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FinancialLiteracyApp
+//
+//  Created by Meghan Sun on 2/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FinancialLiteracyApp/FinancialLiteracyApp/FinancialLiteracyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialLiteracyAppApp.swift
+//  FinancialLiteracyApp
+//
+//  Created by Meghan Sun on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FinancialLiteracyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
