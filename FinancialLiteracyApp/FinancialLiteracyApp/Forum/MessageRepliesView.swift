ea2c7ae91a3ec08d52d00c76bb3566ee6a0e7684
Sadie Amato <saamato@email.unc.edu>
2023-02-11 22:14:45 -0500

    added button to reply in comments of posts!

--- FinancialLiteracyApp/FinancialLiteracyApp/Forum/MessageRepliesView.swift
+++ FinancialLiteracyApp/FinancialLiteracyApp/Forum/MessageRepliesView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 // View of Selected Message and it's replies
 struct MessageRepliesView: View {
+    @State var user_reply: String = ""
     var message: Message
     
     var body: some View {
@@ -26,6 +27,7 @@ struct MessageRepliesView: View {
                 }
                 
                 Spacer()
+                TextField("Add a reply...", text: $user_reply).textFieldStyle(RoundedBorderTextFieldStyle())
             }
             .navigationBarTitle("", displayMode: .inline) // remove spacing for title
             .padding()
@@ -35,10 +37,20 @@ struct MessageRepliesView: View {
         
 }
 
-
-
+struct OvalTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(10)
+            .background(LinearGradient(gradient: Gradient(colors: [Color.orange, Color.orange]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .cornerRadius(20)
+            .shadow(color: .gray, radius: 10)
+    }
+    
+    
+    
 struct MessageRepliesView_Previews: PreviewProvider {
     static var previews: some View {
         MessageRepliesView(message: Message.example_messages[0])
     }
+}
 }
