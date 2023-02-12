75c684fa0922db1b23c3a5d87b19efe1ef46e9d5
Sophie Jiang <sjiang13@email.unc.edu>
2023-02-12 09:56:15 -0500

    finished splash screen!!

--- FinancialLiteracyApp/FinancialLiteracyApp/FinancialLiteracyAppApp.swift
+++ FinancialLiteracyApp/FinancialLiteracyApp/FinancialLiteracyAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FinancialLiteracyAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreenView()
         }
     }
 }
