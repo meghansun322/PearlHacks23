4f9afa6a4ee0885bcaeec51173eff7d7e092e486
Meghan Sun <meghansun322@gmail.com>
2023-02-12 07:37:04 -0500

    removed examples

--- FinancialLiteracyApp/FinancialLiteracyApp/Forum/MessageView.swift
+++ FinancialLiteracyApp/FinancialLiteracyApp/Forum/MessageView.swift
@@ -73,6 +73,6 @@ struct MessageView: View {
 
 struct MessageView_Previews: PreviewProvider {
     static var previews: some View {
-        MessageView(message: Message.example_messages[0])
+        MessageView(message: Message.example_message)
     }
 }
